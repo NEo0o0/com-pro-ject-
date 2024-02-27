c308fcfc19a3e7142222e559ba25b248a6228676
TeetatB <teetatmix36108@gmail.com>
2024-02-27 16:36:08 +0700

    add nf

--- cf.cpp
+++ cf.cpp
@@ -4,12 +4,18 @@
 using namespace std;
 const string dname[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
 const string zyname[] = {"Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat","Monkey","Rooster","Dog","Pig"};
+int frnum[7] = {};
+int srnum[7] = {};
+int trnum[7] = {};
 int calThaiYear(int );
 string findwday(int ,int ,int );
 bool checkly(int );
 int findmly(int ,int );
 int findmnotly(int ,int );
 int calZodiacAnimal(int ,int ,int ,bool );
+int calfnum(int );
+int calsnum(int );
+int caltnum(int );
 
 int main(){
     int dd,mm,yy;
@@ -396,3 +402,57 @@ int calZodiacAnimal(int dd,int mm ,int yy,bool ly){
             }
     }
 }
+
+int calfnum(int dd,int num[7]){
+    switch(dd){
+        case 1:
+            for(int i = 0;i < 7;i++){
+                num[i] = i+1;
+            }
+            break;
+        case 2:
+            for(int i = 0;i < 6;i++){
+                num[i] = i+2;
+            }
+            num[6] = 1;
+            break;
+        case 3:
+            for(int i = 0;i < 5;i++){
+                num[i] = i+3;
+            }
+            for(int j = 0;j < 2;j++){
+                num[5+j] = j+1;
+            }
+            break;
+        case 4:
+            for(int i = 0;i < 4;i++){
+                num[i] = i+4;
+            }
+            for(int j = 0;j < 3;j++){
+                num[4+j] = j+1;
+            }
+            break;
+        case 5:
+            for(int i = 0;i < 3;i++){
+                num[i] = i+5;
+            }
+            for(int j = 0;j < 4;j++){
+                num[3+j] = j+1;
+            }
+            break;
+        case 6:
+            for(int i = 0;i < 2;i++){
+                num[i] = i+6;
+            }
+            for(int j = 0;j < 5;j++){
+                num[2+j] = j+1;
+            }
+            break;
+        case 7:
+            num[0] = 7;
+            for(int i = 0;i < 6;i++){
+                num[1+i] = i+1;
+            }            
+    }
+}
+
